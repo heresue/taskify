1fb0432cddb9878a98838a3beb4cb1cf09f43710
heejin <heejin0857@naver.com>
2025-03-21 03:53:21 +0900

    Style : edit responsive css

--- components/common/Modal/index.tsx
+++ components/common/Modal/index.tsx
@@ -28,10 +28,10 @@ export default function Modal({
   if (!isOpen) return null;
 
   const paddingCSS: Record<PaddingSize, string> = {
-    '64/40': 'px-16 py-10 maxS:px-10 maxS:py-8',
-    '32/32': 'p-8 maxS:px-4 maxS:py-6',
-    '32/24': 'px-8 py-6 maxS:p-4',
-    '24/24': 'p-6 maxS:px-4 maxS:py-6',
+    '64/40': 'px-16 py-10 max-sm:px-10 max-sm:py-8',
+    '32/32': 'p-8 max-sm:px-4 max-sm:py-6',
+    '32/24': 'px-8 py-6 max-sm:p-4',
+    '24/24': 'p-6 max-sm:px-4 max-sm:py-6',
   };
 
   const borderRadiusCSS: Record<RadiusSize, string> = {
@@ -59,15 +59,15 @@ export default function Modal({
         {children}
         <div
           className={`flex gap-[7px] ${
-            cancelMessage
-              ? 'maxS:w-[295px] w-[520px] min-w-[295px] sm:w-[68vw]'
-              : 'maxS:w-48 w-60 min-w-48 sm:w-[31.3vw]'
+            cancelMessage && confirmMessage
+              ? 'w-[520px] min-w-[295px] max-sm:w-[83.9vw]'
+              : 'w-60 min-w-48 max-sm:w-[45.1vw]'
           }`}
         >
           {cancelMessage && (
             <button
               onClick={onClose}
-              className="border-gray100 flex-1 rounded-lg border-solid px-[46px] py-[14px]"
+              className="border-gray300 flex-1 rounded-lg border border-solid px-[46px] py-[14px]"
             >
               {cancelMessage}
             </button>
@@ -75,7 +75,7 @@ export default function Modal({
           {confirmMessage && (
             <button
               onClick={handleConfirmClick}
-              className="flex-1 rounded-lg bg-black px-[46px] py-[14px] text-white"
+              className="bg-violet flex-1 rounded-lg px-[46px] py-[14px] text-white"
             >
               {confirmMessage}
             </button>
