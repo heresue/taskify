5e1053c6939de05ea4287874074d07688486a599
Seok-jun.Kang <kangsj0327@gmail.com>
2025-03-21 16:49:33 +0900

    Style : dropdown trigger, list 스타일 수정 - z-index, 정렬 속성 추가

--- components/common/Dropdown/List.tsx
+++ components/common/Dropdown/List.tsx
@@ -18,7 +18,7 @@ interface SearchableListProps {
 
 export function MenuList({ options, onClickItem }: ListProps) {
   return (
-    <ul className="border-gray300 absolute right-0 mt-1 flex max-h-40 w-fit min-w-24 flex-col items-stretch justify-start gap-1 overflow-y-auto rounded-md border bg-white px-1.5 py-2 drop-shadow-[0px_4px_20px_rgba(0,0,0,0.08)] filter">
+    <ul className="border-gray300 absolute right-0 z-1 mt-1 flex max-h-40 w-fit min-w-24 flex-col items-stretch justify-start gap-1 overflow-y-auto rounded-md border bg-white px-1.5 py-2 drop-shadow-[0px_4px_20px_rgba(0,0,0,0.08)] filter">
       {options.map((option) => (
         <MenuOption key={option.id} option={option} onClickItem={onClickItem} />
       ))}
@@ -28,7 +28,7 @@ export function MenuList({ options, onClickItem }: ListProps) {
 
 export function SelectionList({ options, onClickItem, selected }: SelectionListProps) {
   return (
-    <ul className="border-gray300 absolute right-0 mt-1 flex max-h-68 w-full min-w-24 flex-col items-stretch justify-start gap-1 overflow-y-auto rounded-md border bg-white px-1.5 py-2 drop-shadow-[0px_4px_20px_rgba(0,0,0,0.08)] filter">
+    <ul className="border-gray300 absolute right-0 z-1 mt-1 flex max-h-68 w-full min-w-24 flex-col items-stretch justify-start gap-1 overflow-y-auto rounded-md border bg-white px-1.5 py-2 drop-shadow-[0px_4px_20px_rgba(0,0,0,0.08)] filter">
       {options.map((option) => (
         <SelectionOption
           key={option.id}
@@ -43,7 +43,7 @@ export function SelectionList({ options, onClickItem, selected }: SelectionListP
 
 export function SearchableList({ options, onClickItem, selected }: SearchableListProps) {
   return (
-    <ul className="border-gray300 absolute right-0 mt-1 flex max-h-68 w-full min-w-24 flex-col items-stretch justify-start gap-1 overflow-y-auto rounded-md border bg-white px-1.5 py-2 drop-shadow-[0px_4px_20px_rgba(0,0,0,0.08)] filter">
+    <ul className="border-gray300 absolute right-0 z-1 mt-1 flex max-h-68 w-full min-w-24 flex-col items-stretch justify-start gap-1 overflow-y-auto rounded-md border bg-white px-1.5 py-2 drop-shadow-[0px_4px_20px_rgba(0,0,0,0.08)] filter">
       {options.map((option) => (
         <SearchableOption
           key={option.id}
