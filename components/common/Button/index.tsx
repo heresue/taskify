e19ef3bef2e8c350c598805b9946cf75a98c2797
heresue <sue.synpark@gmail.com>
2025-03-21 13:35:48 +0900

    feat: Button type 속성 추가 (button, submit, reset)

--- components/common/Button/index.tsx
+++ components/common/Button/index.tsx
@@ -4,37 +4,41 @@ import { sizes, variants } from './style';
 interface ButtonProps {
   children?: React.ReactNode;
   onClick?: () => void;
-  disabled?: boolean;
+  type?: 'button' | 'submit' | 'reset';
   variant?: keyof typeof variants;
   size?: keyof typeof sizes | string;
+  fullWidth?: boolean;
   className?: string;
+  disabled?: boolean;
 }
 
 export default function Button({
   children,
   onClick,
-  disabled = false,
+  type = 'button',
   variant = 'solid',
   size = 'button',
+  fullWidth,
   className,
+  disabled = false,
 }: ButtonProps) {
   const selectedSize = sizes[size as keyof typeof sizes] ?? size;
 
   return (
     <button
-      type="button"
+      type={type}
       onClick={onClick}
       disabled={disabled}
       className={clsx(
         'flex cursor-pointer items-center justify-center overflow-hidden p-2 font-medium',
         selectedSize,
         variants[variant],
+        fullWidth && 'w-full',
         disabled && 'cursor-not-allowed',
         className
       )}
     >
-      <div className="overflow-hidden text-ellipsis whitespace-nowrap">{children}
-      </div>
+      <div className="overflow-hidden text-ellipsis whitespace-nowrap">{children}</div>
     </button>
   );
 }
