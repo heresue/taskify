6c6fc6a9e2ec45642e5f3e9c627e72ed153d81f7
Seok-jun.Kang <kangsj0327@gmail.com>
2025-03-21 16:47:37 +0900

    Feat : SearchableDropdownItem 타입 분리

--- components/common/Dropdown/Item.tsx
+++ components/common/Dropdown/Item.tsx
@@ -1,4 +1,4 @@
-import { DropdownItem } from '@/components/common/Dropdown/types';
+import { DropdownItem, SearchableDropdownItem } from '@/components/common/Dropdown/types';
 import CheckIcon from '@/assets/icons/CheckIcon';
 
 interface OptionProps {
@@ -8,6 +8,12 @@ interface OptionProps {
 
 interface SelectionOptionProps extends OptionProps {
   selected: DropdownItem | null;
+}
+
+interface SearchableOptionProps {
+  option: SearchableDropdownItem;
+  onClickItem: (option: SearchableDropdownItem) => void;
+  selected: SearchableDropdownItem | null;
 }
 
 export function MenuOption({ option, onClickItem }: OptionProps) {
@@ -28,6 +34,27 @@ export function MenuOption({ option, onClickItem }: OptionProps) {
 }
 
 export function SelectionOption({ option, onClickItem, selected }: SelectionOptionProps) {
+  return (
+    <li
+      className="hover:bg-violet8 hover:text-violet flex min-h-12 w-full cursor-pointer flex-row items-center justify-start gap-2 rounded-sm px-4 py-1"
+      onClick={() => onClickItem(option)}
+    >
+      <div className={`${option.id !== selected?.id && 'invisible'}`}>
+        <CheckIcon width="22" height="22" />
+      </div>
+
+      <button className="text-regular14 cursor-pointer">
+        {option.renderItem ? (
+          <>{option.renderItem()}</>
+        ) : (
+          <span className="truncate">{option.value}</span>
+        )}
+      </button>
+    </li>
+  );
+}
+
+export function SearchableOption({ option, onClickItem, selected }: SearchableOptionProps) {
   return (
     <li
       className="hover:bg-violet8 hover:text-violet flex min-h-12 w-full cursor-pointer flex-row items-center justify-start gap-2 rounded-sm px-4 py-1"
