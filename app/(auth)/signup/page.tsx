c4ff8961b0e97cad362f88e9b92230fc81736a59
heejin <heejin0857@naver.com>
2025-03-22 19:14:56 +0900

    Style : gap -> margin

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -5,19 +5,17 @@ import SignupForm from '@/components/SignupForm/SignupForm';
 export default function Signup() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <div className="text-black200 flex w-[520px] min-w-[351px] flex-col gap-[30px] max-sm:mx-3">
-        <div className="flex flex-col items-center justify-center gap-2.5 max-sm:gap-2">
+      <div className="text-black200 flex w-[520px] min-w-[351px] flex-col max-sm:mx-3">
+        <div className="mb-[30px] flex flex-col items-center justify-center gap-2.5 max-sm:gap-2">
           <Logo />
           <span className="text-medium20">첫 방문을 환영합니다!</span>
         </div>
-        <div className="flex flex-col gap-6">
-          <SignupForm />
-          <div className="text-regular16 flex items-center justify-center gap-2">
-            <span className="text-black200">이미 회원이신가요?</span>
-            <Link href={'/login'} className="text-violet underline">
-              로그인하기
-            </Link>
-          </div>
+        <SignupForm />
+        <div className="text-regular16 mt-6 flex items-center justify-center gap-2">
+          <span className="text-black200">이미 회원이신가요?</span>
+          <Link href={'/login'} className="text-violet underline">
+            로그인하기
+          </Link>
         </div>
       </div>
     </div>
