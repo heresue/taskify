57c9bf7be55002a9fb2ddb8ede80300da796d934
heejin <heejin0857@naver.com>
2025-03-22 23:06:31 +0900

    Fix : changing auth layout logo png -> svg

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,10 @@
-import Image from 'next/image';
-import Logo from '@/public/logo.png';
+import Logo from '@/public/icons/logo.svg';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center">
       <div className="w-full max-w-130 px-3">
-        <Image className="m-auto mb-2.5" src={Logo} width={200} height={280} alt="Taskify" />
+        <Logo className="m-auto mb-2.5" width={200} height={280} />
         {children}
       </div>
     </main>
