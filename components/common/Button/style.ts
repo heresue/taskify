a6f37d61081376b4de9ae44dbe48b38900631b52
heejin <heejin0857@naver.com>
2025-03-24 15:42:44 +0900

    Fix : delete button width

--- components/common/Button/style.ts
+++ components/common/Button/style.ts
@@ -7,12 +7,11 @@ const sizes = {
   modalAlert: 'h-[42px] text-semi14 md:h-[48px] md:text-semi16 rounded-lg',
   delete: 'w-[52px] h-[32px] text-medium12 md:w-[84px] md:text-medium14 rounded-sm',
   comment: 'w-[84px] h-[28px] text-medium12 md:w-[77px] md:h-[32px] lg:w-[83px] rounded-sm',
-  addTodo: 'w-[284px] h-[32px] md:w-[544px] md:h-[40px] lg:w-[314px] rounded-md',
+  addTodo: 'h-[32px] md:h-[40px] rounded-md',
   addColumn: 'w-[284px] h-[66px] text-bold18 md:w-[544px] md:h-[70px] lg:w-[354px] rounded-lg',
   dashboardCard:
     'h-[58px] text-semi14 md:w-[247px] md:h-[68px] md:text-semi16 lg:w-[332px] lg:h-[70px] rounded-lg',
-  deleteDashboard:
-    'h-[52px] text-medium16 md:w-[320px] md:h-[62px] md:text-medium18 rounded-lg',
+  deleteDashboard: 'h-[52px] text-medium16 md:w-[320px] md:h-[62px] md:text-medium18 rounded-lg',
 };
 
 const variants = {
