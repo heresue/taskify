ab3942187950a5ac0d16b32377bffb1e02a63cbb
Seok-jun.Kang <kangsj0327@gmail.com>
2025-03-24 18:46:45 +0900

    Feat : 로그인 오류 시 Modal 띄우기

--- app/(auth)/login/LoginForm.tsx
+++ app/(auth)/login/LoginForm.tsx
@@ -2,6 +2,7 @@
 
 import FormField from '@/components/compound/form/FormField';
 import Button from '@/components/common/Button';
+import Modal from '@/components/common/Modal';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { validateEmail, validatePassword } from '@/utils/authValidate';
@@ -13,12 +14,14 @@ export default function LoginForm() {
   const [isEmailValid, setIsEmailValid] = useState(false);
   const [isPasswordValid, setIsPasswordValid] = useState(false);
   const [canSubmit, setCanSubmit] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
 
-  const handleEmailBlur = (e) => {
+  const handleEmailBlur = (e: React.FocusEvent<HTMLInputElement>) => {
     setIsEmailValid(validateEmail(e.target.value));
   };
 
-  const handlePasswordBlur = (e) => {
+  const handlePasswordBlur = (e: React.FocusEvent<HTMLInputElement>) => {
     setIsPasswordValid(validatePassword(e.target.value));
   };
 
@@ -42,41 +45,53 @@ export default function LoginForm() {
     if (result.success) {
       router.push('/mydashboard');
     } else {
-      alert(result.message);
+      setModalMessage(result.message);
+      setIsModalOpen(true);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex w-full flex-col gap-4 lg:gap-6">
-      <div className="flex flex-col gap-2 lg:gap-4">
-        <FormField
-          id="email"
-          name="email"
-          fieldType="input"
-          label="이메일"
-          type="email"
-          errorMessage="이메일 형식으로 작성해 주세요."
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          onBlur={handleEmailBlur}
-          isValid={isEmailValid}
-        />
-        <FormField
-          id="password"
-          name="password"
-          fieldType="input"
-          label="비밀번호"
-          type="password"
-          errorMessage="8자 이상 작성해 주세요."
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          onBlur={handlePasswordBlur}
-          isValid={isPasswordValid}
-        />
-      </div>
-      <Button size="auth" fullWidth={true} type="submit" disabled={!canSubmit}>
-        로그인
-      </Button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="flex w-full flex-col gap-4 lg:gap-6">
+        <div className="flex flex-col gap-2 lg:gap-4">
+          <FormField
+            id="email"
+            name="email"
+            fieldType="input"
+            label="이메일"
+            type="email"
+            errorMessage="이메일 형식으로 작성해 주세요."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            onBlur={handleEmailBlur}
+            isValid={isEmailValid}
+          />
+          <FormField
+            id="password"
+            name="password"
+            fieldType="input"
+            label="비밀번호"
+            type="password"
+            errorMessage="8자 이상 작성해 주세요."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onBlur={handlePasswordBlur}
+            isValid={isPasswordValid}
+          />
+        </div>
+        <Button size="auth" fullWidth={true} type="submit" disabled={!canSubmit}>
+          로그인
+        </Button>
+      </form>
+      <Modal
+        isOpen={isModalOpen}
+        padding="64/40"
+        borderRadius="16"
+        submitMessage="확인"
+        onClose={() => setIsModalOpen(false)}
+      >
+        {modalMessage}
+      </Modal>
+    </>
   );
 }
