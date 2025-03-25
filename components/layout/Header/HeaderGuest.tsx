5dd1054a55ba54a5110daf3c4b2cc1d89d96f8e5
Seok-jun.Kang <kangsj0327@gmail.com>
2025-03-25 16:28:32 +0900

    Fix : HeaderGuest 스타일 수정 - height 반응형 속성 - header 상단 고정(position: sticky)

--- components/layout/Header/HeaderGuest.tsx
+++ components/layout/Header/HeaderGuest.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function HeaderGuest() {
   return (
-    <header className="flex h-17.5 items-center justify-between bg-black px-6 md:px-10 lg:px-17.5">
+    <header className="sticky top-0 flex h-15 items-center justify-between bg-black px-6 md:h-17.5 md:px-10 lg:px-17.5">
       <Link href="/" className="hidden md:block">
         <Logo color="white" size="md" />
       </Link>
