73773d79eba6115ad1ca6d448ff1e2e7015be5f7
Seok-jun.Kang <kangsj0327@gmail.com>
2025-03-25 17:46:48 +0900

    Chore : HeaderUser 컴포넌트 정의

---
+++ components/layout/Header/HeaderUser.tsx
@@ -0,0 +1,8 @@
+export default function HeaderUser() {
+  return (
+    <header className="border-b-gray300 sticky top-0 flex h-15 items-center justify-between border-b-1 bg-white px-3 md:h-17.5 md:px-10 lg:px-20">
+      <div>비브리지</div>
+      <div></div>
+    </header>
+  );
+}
