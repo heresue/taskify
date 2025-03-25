db7c109c1c7e7f30f1bf29bf83864dbbf496116e
heresue <sue.synpark@gmail.com>
2025-03-25 18:08:52 +0900

    style: dashboard page 배경색 layout 적용

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen">
       <SideNav />
-      <main className="flex-1 overflow-y-auto">{children}</main>
+      <main className="bg-gray100 flex-1 overflow-y-auto">{children}</main>
     </div>
   );
 }
