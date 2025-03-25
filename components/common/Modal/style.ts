61e74c4877c1abee2ad7f341ae50d0ab97ad5729
heejin <heejin0857@naver.com>
2025-03-26 00:26:00 +0900

    Fix : cancel modal responsive css

--- components/common/Modal/style.ts
+++ components/common/Modal/style.ts
@@ -2,10 +2,10 @@ export type PaddingSize = '64/40' | '24/24' | '32/32' | '32/24';
 export type RadiusSize = '16' | '8';
 
 export const paddingCSS: Record<PaddingSize, string> = {
-  '64/40': 'px-10 py-8 sm:px-16 sm:py-10',
-  '32/32': 'px-4 py-6 sm:p-8',
-  '32/24': 'p-4 sm:px-8 sm:py-6',
-  '24/24': 'px-4 py-6 sm:p-6',
+  '64/40': 'px-10 py-8 md:px-16 md:py-10',
+  '32/32': 'px-4 py-6 md:p-8',
+  '32/24': 'p-4 md:px-8 md:py-6',
+  '24/24': 'px-4 py-6 md:p-6',
 };
 
 export const borderRadiusCSS: Record<RadiusSize, string> = {
