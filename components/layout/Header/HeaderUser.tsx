7255e8246275f87f1935acdad26d9d994a002c7f
Seok-jun.Kang <kangsj0327@gmail.com>
2025-03-26 01:33:00 +0900

    HeaderUser - title 레이아웃 추가

--- components/layout/Header/HeaderUser.tsx
+++ components/layout/Header/HeaderUser.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import AddBoxGrayIcon from '@/assets/icons/AddBoxGrayIcon';
 import UserBadge from '@/components/UserBadge/UserBadge';
-import MemberBadgeList from './MemberBadgeList';
+import MemberBadgeList from '@/components/layout/Header/MemberBadgeList';
 
 interface Member {
   id: number;
@@ -14,21 +15,7 @@ interface Member {
   isOwner: boolean;
 }
 
-const members0: Member[] = [];
-
-const members1: Member[] = [
-  {
-    id: 0,
-    userId: 0,
-    email: 'a@gmail.com',
-    nickname: 'seokjun',
-    profileImageUrl: 'string',
-    createdAt: '2025-03-25T09:49:49.827Z',
-    updatedAt: '2025-03-25T09:49:49.827Z',
-    isOwner: true,
-  },
-];
-
+// membersN 은 mockData입니다.
 const members3: Member[] = [
   {
     id: 0,
@@ -135,49 +122,44 @@ const members7: Member[] = [
   },
 ];
 
-/* 
-  요청 /{teamId}/members/size=4
-  /members 대시보드 멤버 목록 조회 응답 결과 예시
-  {
-  "members": [
-    {
-      "id": 0,
-      "userId": 0,
-      "email": "string",
-      "nickname": "string",
-      "profileImageUrl": "string",
-      "createdAt": "2025-03-25T14:14:20.740Z",
-      "updatedAt": "2025-03-25T14:14:20.740Z",
-      "isOwner": true
-    }
-  ],
-  "totalCount": 0
-  }
- */
 export default function HeaderUser() {
+  // TODO: dashboardId 불러오기 : useParams() 활용
+  // TODO: 대시보드 정보 불러오기 : fetch()  /dashboards/{dashboardId}
+  // TODO: MemberBadgeList에 전달할 대시보드 멤버 목록 불러오기. : fetch() /members?page=1&size=4&dashboardId=123 -> members 배열, totalCount
   const dashboardId = 1;
+  const title = '비브리지';
+  const createdByMe = true;
+
   return (
     <header className="border-b-gray300 sticky top-0 flex h-15 items-center justify-between border-b-1 bg-white px-3 md:h-17.5 md:px-10 lg:px-20">
-      <div className="invisible lg:visible lg:block">비브리지</div>
+      <div className="invisible lg:visible lg:flex lg:gap-2">
+        <span className="text-bold20 select-none">{title}</span>
+        {createdByMe && (
+          <Image src="/icons/crown.svg" width={20} height={16} alt="내가 만든 대시보드" />
+        )}
+      </div>
       <div className="flex items-center">
         <div className="flex gap-1.5 md:gap-3">
-          <Link // TODO: 태블릿, PC 화면 톱니바퀴 아이콘 넣기
+          {/* TODO: 태블릿, PC 화면 톱니바퀴 아이콘 넣기 */}
+          <Link
             href={`/dashboard/${dashboardId}/edit`}
-            className="text-medium14 text-gray500 border-gray300 flex items-center gap-2 rounded-md border-1 px-3 py-1.5 md:rounded-lg md:px-4 md:py-2"
+            className="text-medium14 text-gray500 border-gray300 flex items-center gap-2 truncate rounded-md border-1 px-3 py-1.5 md:rounded-lg md:px-4 md:py-2"
           >
             관리
           </Link>
-          <Link // FIXME: 클릭 시 edit 페이지로 이동하는게 아니라, 초대 모달이 떠야 합니다.
+          {/* FIXME: 클릭 시 edit 페이지로 이동하는게 아니라, 초대 모달을 띄워야 함 */}
+          <Link
             href={`/dashboard/${dashboardId}/edit`}
-            className="text-medium14 text-gray500 border-gray300 flex items-center gap-2 rounded-md border-1 px-3 py-1.5 md:rounded-lg md:px-4 md:py-2"
+            className="text-medium14 text-gray500 border-gray300 flex items-center gap-2 truncate rounded-md border-1 px-3 py-1.5 md:rounded-lg md:px-4 md:py-2"
           >
             <AddBoxGrayIcon width={16} height={16} className="hidden md:block" />
             초대하기
           </Link>
         </div>
         <MemberBadgeList members={members7} totalCount={members7.length} />
-        {/* FIXME: Badge 유저명 텍스트 색상 black200으로 수정 필요 */}
         <div className="bg-gray300 mx-3 h-8.5 w-[1px] md:mx-6 md:h-[9.5] lg:mx-8"></div>
+        {/* TODO: 사용자명 불러오기. 배유철씨 아님 : 로그인 시 유저 이름도 브라우저 저장소에 저장해야함! */}
+        {/* FIXME: Badge 유저명 텍스트 색상 black200으로 수정 필요 */}
         <UserBadge size={38} gap={12} userName="배유철" fontSize="M16" responsive={true} />
       </div>
     </header>
