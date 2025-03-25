86e2b2464ebda570d82d25cad17df22344fe8fda
heejin <heejin0857@naver.com>
2025-03-26 00:55:50 +0900

    Feat : separating to do form

--- components/ToDoFormModal/ToDoFormModal.tsx
+++ components/ToDoFormModal/ToDoFormModal.tsx
@@ -1,9 +1,5 @@
-import ColumnName from '../ColumnName/ColumnName';
-import { SearchableDropdown, SelectionDropdown } from '../common/Dropdown';
 import Modal from '../common/Modal';
-import FormField from '../compound/form/FormField';
-import UploadImage from '../compound/upload/UploadImage';
-import UserBadge from '../UserBadge/UserBadge';
+import ToDoForm from './ToDoForm';
 
 interface ToDoFormProps {
   open: boolean;
@@ -24,44 +20,7 @@ export default function ToDoFormModal({ open, onClose, cardId }: ToDoFormProps) 
     >
       <div className="flex w-full flex-col gap-8">
         <h1 className="text-bold24 text-black200">할 일 {CreateOrUpdate}</h1>
-        <div className="flex flex-col gap-8 md:flex-row">
-          {cardId && (
-            <div className="flex flex-1 flex-col gap-2">
-              <label className="text-black200 text-medium18">상태</label>
-              <SelectionDropdown
-                options={[
-                  { value: 'to do', id: 1, renderItem: () => <ColumnName columnName="todo" /> },
-                ]}
-                onSelect={() => {}}
-              />
-            </div>
-          )}
-          <div className="flex flex-1 flex-col gap-2">
-            <label className="text-black200 text-medium18">담당자</label>
-            <SearchableDropdown
-              options={[
-                {
-                  value: '김희진',
-                  id: 1,
-                  renderItem: () => (
-                    <UserBadge size={26} userName="김희진" gap={6} fontSize="R14" />
-                  ),
-                },
-              ]}
-              onSelect={() => {}}
-              placeholder="이름을 입력해 주세요"
-            />
-          </div>
-        </div>
-        <FormField fieldType="input" label="제목" placeholder="제목을 입력해 주세요" required />
-        <FormField fieldType="textarea" label="설명" placeholder="설명을 입력해 주세요" required />
-        <FormField fieldType="input" label="태그" placeholder="입력 후 Enter" />
-        <div className="flex flex-col gap-[5px]">
-          <label className="text-black200 text-medium18">이미지</label>
-          <div className="relative h-[76px] w-[76px]">
-            <UploadImage image={''} id="" onChange={() => {}} />
-          </div>
-        </div>
+        <ToDoForm cardId={cardId} />
       </div>
     </Modal>
   );
