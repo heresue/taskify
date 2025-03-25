fc8ca3ac6abc6d6b19c9d68db0afba761a22e4e9
heejin <heejin0857@naver.com>
2025-03-26 02:18:41 +0900

    Style : dashboardId page UI

--- app/(dashboard)/dashboard/[dashboardId]/page.tsx
+++ app/(dashboard)/dashboard/[dashboardId]/page.tsx
@@ -1,9 +1,19 @@
+import Button from '@/components/common/Button';
 import DashboardColumn from '@/components/Dashboard/DashboardColumn/DashboardColum';
+import Plus from '@/public/icons/plus.svg';
 
 export default function DashboardId() {
   return (
-    <div className="h-full">
+    <div className="flex h-full">
       <DashboardColumn />
+      <div className="fixed inset-x-3 bottom-7 lg:static lg:mx-5 lg:mt-[68px]">
+        <Button size="addColumn" variant="outline">
+          <div className="text-bold18 text-black200 flex gap-3">
+            새로운 칼럼 추가하기
+            <Plus />
+          </div>
+        </Button>
+      </div>
     </div>
   );
 }
