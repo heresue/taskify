600b25b68c3c9568ba5b688b2a8824ee79ebb26e
heresue <sue.synpark@gmail.com>
2025-03-26 14:05:19 +0900

    fix: 오타 수정

--- app/(dashboard)/dashboard/[id]/edit/DashboardNameSection .tsx
+++ app/(dashboard)/dashboard/[id]/edit/DashboardNameSection .tsx
@@ -7,7 +7,7 @@ export default function DashboardNameSection() {
     <div id="section" className="rounded-2xl bg-white px-[28px] py-[32px]">
       <h3 className="text-bold24 mb-6">대시보드 이름</h3>
       <label htmlFor="dashboardName" className="mb-4 flex flex-col gap-2">
-        <span className="text-medium18 text-bla ck200">대시보드 이름</span>
+        <span className="text-medium18 text-black200">대시보드 이름</span>
         <Input id="dashboardName" name="dashboardName" placeholder="이름을 입력하세요" />
       </label>
       <div className="mb-6 flex gap-2">
