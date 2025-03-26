91571d096f3fa10672589aa4a6f3a1715820deb9
heejin <heejin0857@naver.com>
2025-03-26 20:32:13 +0900

    Fix : build error

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,11 @@
-import Link from 'next/link';
-import Image from 'next/image';
+// import Link from 'next/link';
+// import Image from 'next/image';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center">
       <div className="w-full max-w-130 px-3">
-        <Link href="/" title="홈으로 이동">
+        {/* <Link href="/" title="홈으로 이동">
           <Image
             src="/icons/logo.svg"
             className="m-auto mb-2.5"
@@ -13,7 +13,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             height={280}
             alt="logo"
           />
-        </Link>
+        </Link> */}
         {children}
       </div>
     </main>
