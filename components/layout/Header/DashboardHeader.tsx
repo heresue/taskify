6cffaf8fee324fff074b137de63b5318e841690a
Seok-jun.Kang <kangsj0327@gmail.com>
2025-03-27 14:57:07 +0900

    Feat : Header - 사용자 이름, 프로필 이미지 출력

--- components/layout/Header/DashboardHeader.tsx
+++ components/layout/Header/DashboardHeader.tsx
@@ -43,7 +43,7 @@ export default async function DashboardHeader({ dashboardId }: { dashboardId: nu
 
   const cookieStore = await cookies();
 
-  const { nickname } = JSON.parse(cookieStore.get('userInfo')?.value || '');
+  const { nickname, profileImageUrl } = JSON.parse(cookieStore.get('userInfo')?.value || '');
 
   return (
     <header className="border-b-gray300 sticky top-0 flex h-15 items-center justify-between border-b-1 bg-white px-3 md:h-17.5 md:px-10 lg:px-20">
@@ -74,7 +74,14 @@ export default async function DashboardHeader({ dashboardId }: { dashboardId: nu
         <MemberBadgeList members={members} totalCount={totalCount} />
         <div className="bg-gray300 mx-3 h-8.5 w-[1px] md:mx-6 md:h-[9.5] lg:mx-8"></div>
         {/* FIXME: Badge 유저명 텍스트 색상 black200으로 수정 필요 */}
-        <UserBadge size={38} gap={12} userName={nickname} fontSize="M16" responsive={true} />
+        <UserBadge
+          size={38}
+          gap={12}
+          userName={nickname}
+          profile={profileImageUrl}
+          fontSize="M16"
+          responsive={true}
+        />
       </div>
     </header>
   );
