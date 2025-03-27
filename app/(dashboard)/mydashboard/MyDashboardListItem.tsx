5fe4424cc436f9e6cff124c61c826c5a2d3e9567
heresue <sue.synpark@gmail.com>
2025-03-28 04:08:50 +0900

    feat: 내 대시보드 세부 UI 및 페이지 이동 기능 적용

---
+++ app/(dashboard)/mydashboard/MyDashboardListItem.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import DashboardColorIcon, { ColorKey } from '@/components/DashboardColorIcon/DashboardColorIcon';
+import clsx from 'clsx';
+
+interface DashboardListItemProps {
+  dashboardId: number;
+  title: string;
+  colorKey: ColorKey;
+  createdAt?: string;
+  updatedAt?: string;
+  createdByMe: boolean;
+  userId?: number;
+  isSelected?: boolean;
+}
+
+export default function MyDashboardListItem({
+  dashboardId,
+  title,
+  colorKey,
+  createdByMe,
+}: DashboardListItemProps) {
+  return (
+    <Link
+      href={`/dashboard/${dashboardId}`}
+      className="border-gray300 h-[58px] rounded-lg border bg-white md:h-[68px] md:w-[247px] lg:h-[70px] lg:w-[332px]"
+    >
+      <div className="flex items-center gap-4 p-5">
+        <DashboardColorIcon colorKey={colorKey} />
+        <div className="flex hidden min-w-0 flex-1 items-center gap-[6px] md:flex">
+          <span
+            className={clsx(
+              'text-black200 text-semi14 md:text-semi16 truncate',
+              createdByMe ? 'max-w-[calc(100%-14px)]' : 'flex-1'
+            )}
+          >
+            {title}
+          </span>
+          {createdByMe && (
+            <Image
+              src="/icons/crown.svg"
+              alt="내가 만든 대시보드"
+              width={14}
+              height={14}
+              className="shrink-0"
+            />
+          )}
+        </div>
+
+        <Image src="/icons/arrowRight.svg" alt="대시보드로 이동" width={7} height={14} />
+      </div>
+    </Link>
+  );
+}
