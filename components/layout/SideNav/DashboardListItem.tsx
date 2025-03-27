845b68c8eff665313b2c1c7bac6ec6e42416d624
heresue <sue.synpark@gmail.com>
2025-03-28 02:25:35 +0900

    refactor: DashboardListItem 컴포넌트에서 <li> 태그를 부모로 위임하여 재사용성 향상

--- components/layout/SideNav/DashboardListItem.tsx
+++ components/layout/SideNav/DashboardListItem.tsx
@@ -22,32 +22,33 @@ export default function DashboardListItem({
   isSelected,
 }: DashboardListItemProps) {
   return (
-    <li className={clsx('rounded-sm', isSelected ? 'bg-violet8' : 'hover:bg-gray100')}>
-      <Link
-        href={`/dashboard/${dashboardId}`}
-        className="flex h-[50px] items-center gap-4 px-3 py-2"
-      >
-        <DashboardColorIcon colorKey={colorKey} />
-        <div className="flex hidden min-w-0 items-center gap-[6px] md:flex">
-          <span
-            className={clsx(
-              'text-medium18 text-gray500 truncate',
-              createdByMe ? 'max-w-[calc(100%-14px)]' : 'flex-1'
-            )}
-          >
-            {title}
-          </span>
-          {createdByMe && (
-            <Image
-              src="/icons/crown.svg"
-              alt="내가 만든 대시보드"
-              width={14}
-              height={14}
-              className="shrink-0"
-            />
+    <Link
+      href={`/dashboard/${dashboardId}/edit`}
+      className={clsx(
+        'flex h-[50px] items-center justify-center gap-4 rounded-sm px-3 py-2 md:justify-start',
+        isSelected ? 'bg-violet8' : 'hover:bg-gray100'
+      )}
+    >
+      <DashboardColorIcon colorKey={colorKey} />
+      <div className="flex hidden min-w-0 items-center gap-[6px] md:flex">
+        <span
+          className={clsx(
+            'text-medium18 text-gray500 truncate',
+            createdByMe ? 'max-w-[calc(100%-14px)]' : 'flex-1'
           )}
-        </div>
-      </Link>
-    </li>
+        >
+          {title}
+        </span>
+        {createdByMe && (
+          <Image
+            src="/icons/crown.svg"
+            alt="내가 만든 대시보드"
+            width={14}
+            height={14}
+            className="shrink-0"
+          />
+        )}
+      </div>
+    </Link>
   );
 }
