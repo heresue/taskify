8de21c5a5361386057c0694c858204a2200cbdac
heejin <heejin0857@naver.com>
2025-03-28 01:13:02 +0900

    Feat : get dashboard members

--- components/ToDoFormModal/ToDoForm.tsx
+++ components/ToDoFormModal/ToDoForm.tsx
@@ -1,11 +1,57 @@
+'use client';
+
+import { useEffect, useMemo, useState } from 'react';
 import ColumnName from '../ColumnName/ColumnName';
 import { SearchableDropdown, SelectionDropdown } from '../common/Dropdown';
 import FormField from '../compound/form/FormField';
 import UploadImage from '../compound/upload/UploadImage';
 import UserBadge from '../UserBadge/UserBadge';
 import TagInput from './TagInput';
+import { getMembers, Member } from './action';
+import { useParams } from 'next/navigation';
+
+const INITIAL_MEMBER_VALUE = {
+  id: 0,
+  nickname: '',
+  profileImageUrl: '',
+  userId: 0,
+};
 
 export default function ToDoForm({ cardId }: { cardId?: number }) {
+  const [dashboardMembers, setDashboardMembers] = useState<Member[]>([INITIAL_MEMBER_VALUE]);
+  const params = useParams<{ dashboardId: string }>();
+  const dashboardId = Number(params.dashboardId);
+
+  useEffect(() => {
+    if (!dashboardId) return;
+
+    const getMembersData = () => {
+      getMembers(dashboardId)
+        .then((result) => {
+          if (!result) return;
+          setDashboardMembers(result.members);
+        })
+        .catch((err) => console.error(err));
+    };
+    getMembersData();
+  }, [dashboardId]);
+
+  const memberList = useMemo(() => {
+    return dashboardMembers.map((member) => ({
+      value: member.nickname,
+      id: member.id,
+      renderItem: () => (
+        <UserBadge
+          size={26}
+          profile={member.profileImageUrl}
+          userName={member.nickname}
+          gap={6}
+          fontSize="R14"
+        />
+      ),
+    }));
+  }, [dashboardMembers]);
+
   return (
     <>
       <div className="flex flex-col gap-8 md:flex-row">
@@ -23,13 +69,7 @@ export default function ToDoForm({ cardId }: { cardId?: number }) {
         <div className="flex flex-1 flex-col gap-2">
           <label className="text-black200 text-medium18">담당자</label>
           <SearchableDropdown
-            options={[
-              {
-                value: '김희진',
-                id: 1,
-                renderItem: () => <UserBadge size={26} userName="김희진" gap={6} fontSize="R14" />,
-              },
-            ]}
+            options={memberList}
             onSelect={() => {}}
             placeholder="이름을 입력해 주세요"
           />
