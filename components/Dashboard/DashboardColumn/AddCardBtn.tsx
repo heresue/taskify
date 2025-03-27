b4e89183d6de77dc03008207ab28c8a8fa7f37e8
heejin <heejin0857@naver.com>
2025-03-28 03:05:44 +0900

    Feat : post to do card

--- components/Dashboard/DashboardColumn/AddCardBtn.tsx
+++ components/Dashboard/DashboardColumn/AddCardBtn.tsx
@@ -5,11 +5,11 @@ import ToDoFormModal from '@/components/ToDoFormModal/ToDoFormModal';
 import Plus from '@/public/icons/plus.svg';
 import { useState } from 'react';
 
-export default function AddCardBtn() {
+export default function AddCardBtn({ columnId }: { columnId: number }) {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <ToDoFormModal open={isOpen} onClose={() => setIsOpen(false)} />
+      <ToDoFormModal open={isOpen} onClose={() => setIsOpen(false)} columnId={columnId} />
       <Button onClick={() => setIsOpen(true)} fullWidth size="addTodo" variant="outline">
         <Plus />
       </Button>
