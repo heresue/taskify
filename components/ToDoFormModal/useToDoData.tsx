23aef120e60e4683dcf2daf9ecd146043bd68138
heejin <heejin0857@naver.com>
2025-03-28 20:11:34 +0900

    Feat - formatting date - handle image change function

--- components/ToDoFormModal/useToDoData.tsx
+++ components/ToDoFormModal/useToDoData.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { api } from '@/lib/api';
 import { DropdownItem } from '../common/Dropdown/types';
+import { postDashboardCardImage } from './action';
 
 interface ToDoData {
   title: string;
@@ -32,6 +33,20 @@ export default function useToDoData(columnId: number, dashboardId: number) {
     }));
   };
 
+  const formatterDate = (dateValue: Date | null): string => {
+    if (!dateValue) return '';
+
+    const date = new Date(dateValue);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  };
+
   const handleDueDateChange = (date: Date | null) => {
     setToDoData((prev) => ({
       ...prev,
@@ -42,26 +57,44 @@ export default function useToDoData(columnId: number, dashboardId: number) {
   const handleAssigneeUserChange = (userId: number | string) =>
     setAssigneeUser({ ...assigneeUser, id: userId });
 
-  const handleTagsChange = (tags: string[]) => {
-    setTags(tags);
+  const handleTagsChange = (tags: string[]) => setTags(tags);
+
+  const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    const file = e.target.files[0];
+
+    try {
+      const data = await postDashboardCardImage(columnId, file);
+
+      setToDoData((prev) => ({
+        ...prev,
+        imageUrl: data.imageUrl,
+      }));
+    } catch (err) {
+      console.error(err);
+    }
   };
 
-  const handleToDoSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    await api.post('/card', {
-      ...toDoData,
+  const handleToDoSubmit = async () => {
+    await api.post('/cards', {
+      title: toDoData.title,
+      description: toDoData.description,
+      imageUrl: toDoData.imageUrl,
+      dueDate: formatterDate(toDoData.dueDate),
       tags,
       dashboardId,
-      assigneeUser,
+      assigneeUserId: assigneeUser.id,
       columnId,
     });
   };
 
   return {
+    dueDate: toDoData.dueDate,
+    image: toDoData.imageUrl,
     handleFormChange,
     handleAssigneeUserChange,
+    handleImageChange,
     handleDueDateChange,
-    dueDate: toDoData.dueDate,
     handleTagsChange,
     handleToDoSubmit,
   };
