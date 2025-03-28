fc8b26c6bdc5c813698b834eb5920f1e2699585a
heresue <sue.synpark@gmail.com>
2025-03-29 06:18:01 +0900

    style: pagination button css

--- components/Pagination/PaginationControls.tsx
+++ components/Pagination/PaginationControls.tsx
@@ -1,4 +1,5 @@
-import { PaginationControlsProps } from "./type";
+import { PaginationControlsProps } from './type';
+import ArrowIcon from '@/assets/icons/ArrowIcon';
 
 export default function PaginationControls({
   currentPage,
@@ -10,18 +11,33 @@ export default function PaginationControls({
   if (totalPages <= 1) return null;
 
   return (
-    <div className="mt-4 flex items-center justify-end gap-2">
+    <div className="flex items-center justify-end gap-4">
       {showPageInfo && (
-        <span className="text-gray400 text-sm">
-          {currentPage} 페이지 중 {totalPages}
+        <span className="text-black200 text-regular14">
+          {totalPages} 페이지 중 {currentPage}
         </span>
       )}
-      <button onClick={goToPrev} disabled={currentPage === 1}>
-        &lt;
-      </button>
-      <button onClick={goToNext} disabled={currentPage === totalPages}>
-        &gt;
-      </button>
+      <div className="flex">
+        <button
+          className="border-gray300 flex h-10 w-10 items-center justify-center rounded-l-sm border bg-white"
+          onClick={goToPrev}
+          disabled={currentPage === 1}
+        >
+          <ArrowIcon width="7" height="12" color={currentPage === 1 ? '#D9D9D9' : '#787486'} />
+        </button>
+        <button
+          className="border-gray300 flex h-10 w-10 items-center justify-center rounded-r-sm border bg-white"
+          onClick={goToNext}
+          disabled={currentPage === totalPages}
+        >
+          <ArrowIcon
+            direction="right"
+            width="7"
+            height="12"
+            color={currentPage === totalPages ? '#D9D9D9' : '#787486'}
+          />
+        </button>
+      </div>
     </div>
   );
 }
