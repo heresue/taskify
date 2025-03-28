1f37023281ac0d29d82fa9d4abc1a2c16be3ca4b
heresue <sue.synpark@gmail.com>
2025-03-28 16:11:01 +0900

    feat: 초대내역 불러오기 및 취소 기능 구현 - mock data 기반

--- app/(dashboard)/dashboard/[dashboardId]/edit/page.tsx
+++ app/(dashboard)/dashboard/[dashboardId]/edit/page.tsx
@@ -3,15 +3,19 @@ import BackLink from './BackLink';
 import DashboardNameSection from './DashboardNameSection ';
 import MemberListSection from './MemberListSection ';
 import InvitationListSection from './InvitationListSection ';
-
-interface Props {
-  params: {
-    dashboardId: string;
-  };
-}
+import { mockInvitations } from '@/mocks/invitations';
 
 export default async function DashboardIdEdit({ params }: Props) {
   const { dashboardId } = params;
+
+  // mock 데이터
+  // const res = await api.get(`/invitations`);
+  // const allInvitations = res.invitations;
+  const allInvitations = mockInvitations;
+
+  const filteredInvitees = allInvitations.filter(
+    (inv) => inv.dashboard.id === Number(dashboardId) && inv.inviteAccepted === null
+  );
 
   return (
     <div id="wrapper" className="m-5 mb-[57px] flex flex-col gap-[34px]">
@@ -19,8 +23,8 @@ export default async function DashboardIdEdit({ params }: Props) {
 
       <div className="flex w-[620px] flex-col gap-4">
         <DashboardNameSection />
-        <MemberListSection />
-        <InvitationListSection />
+        <MemberListSection initialMembers={allInvitations} />
+        <InvitationListSection dashboardId={dashboardId} />
 
         <Button variant="outline" size="deleteDashboard">
           대시보드 삭제하기
