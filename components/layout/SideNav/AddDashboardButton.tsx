e9c7ab014e76bc7e39a3a89c02448bb84845c13f
heresue <sue.synpark@gmail.com>
2025-03-28 17:15:30 +0900

    style: AddBoxIcon 컴포넌트 관련 코드 수정

--- components/layout/SideNav/AddDashboardButton.tsx
+++ components/layout/SideNav/AddDashboardButton.tsx
@@ -1,13 +1,13 @@
 import CreateDashboardModal from '@/app/(dashboard)/mydashboard/CreateDashboardModal';
+import AddBoxIcon from '@/assets/icons/AddBoxIcon';
 import { useModal } from '@/hooks/useModal';
-import Image from 'next/image';
 
 export default function AddDashboardButton() {
   const { isOpen, open, close } = useModal();
   return (
     <>
       <button type="button" className="m-[3px] mx-auto md:mr-0" onClick={open}>
-        <Image src="/icons/addbox.svg" alt="대시보드 추가" width={14} height={14} />
+        <AddBoxIcon width="14" height="14" />
       </button>
 
       <CreateDashboardModal isOpen={isOpen} onClose={close} />
