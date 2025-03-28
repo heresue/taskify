30ce941eae9e1c35f94047986f3cc2b7878cc885
heejin <heejin0857@naver.com>
2025-03-28 20:05:19 +0900

    Feat : post dashboard card image api

--- components/ToDoFormModal/action.ts
+++ components/ToDoFormModal/action.ts
@@ -1,4 +1,5 @@
 import { api } from '@/lib/api';
+import { getItem } from '@/utils/localstorage';
 
 export interface Member {
   id: number;
@@ -15,4 +16,24 @@ interface MembersPromise {
 export async function getMembers(dashboardId: number) {
   const response = await api.get<MembersPromise>(`/members?dashboardId=${dashboardId}`);
   return response;
+}
+
+export async function postDashboardCardImage(columnId: number, file: File) {
+  const imageFormData = new FormData();
+  imageFormData.append('image', file);
+
+  const accessToken = getItem('accessToken');
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/columns/${columnId}/card-image`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: accessToken ? `Bearer ${accessToken}` : '',
+      },
+      body: imageFormData,
+    }
+  );
+
+  return response.json();
 }
