d285ccd81aaa51ce091512f59ced228333f3f184
heresue <sue.synpark@gmail.com>
2025-03-29 09:58:13 +0900

    feat: pagination 적용 - dashboard member list

--- app/(dashboard)/dashboard/[dashboardId]/edit/MemberListSection .tsx
+++ app/(dashboard)/dashboard/[dashboardId]/edit/MemberListSection .tsx
@@ -3,6 +3,8 @@
 import Button from '@/components/common/Button';
 import UserIcon from '@/assets/icons/UserIcon';
 import { useState } from 'react';
+import PaginationItems from '@/components/Pagination/PagigationItems';
+import PaginationControls from '@/components/Pagination/PaginationControls';
 
 interface Member {
   id: number;
@@ -18,10 +20,22 @@ const mockMembers = [
   { id: 1, nickname: '홍길동' },
   { id: 2, nickname: '김개발자' },
   { id: 3, nickname: '이테스트' },
+  { id: 4, nickname: '박동길' },
+  { id: 5, nickname: '이수박' },
+  { id: 6, nickname: '김자갈' },
+  { id: 7, nickname: '강바울' },
+  { id: 8, nickname: '한폴리' },
+  { id: 9, nickname: '주백호' },
 ];
 
 export default function MemberListSection({ initialMembers = mockMembers }: Props) {
   const [members, setMembers] = useState<Member[]>(initialMembers || []);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 3;
+  const totalPages = Math.ceil(mockMembers.length / itemsPerPage);
+
+  const goToPrev = () => setCurrentPage((prev) => Math.max(prev - 1, 1));
+  const goToNext = () => setCurrentPage((prev) => Math.min(prev + 1, totalPages));
 
   // TODO: 데이터 연동 후 수정
   const deleteMember = async (memberId: number) => {
@@ -42,24 +56,44 @@ export default function MemberListSection({ initialMembers = mockMembers }: Prop
     <div id="section" className="rounded-2xl bg-white pt-[32px]">
       <div className="mb-[27px] flex items-center justify-between px-[28px]">
         <h3 className="text-bold24">구성원</h3>
-        <div>페이지네이션 버튼</div>
+        <PaginationControls
+          currentPage={currentPage}
+          totalPages={totalPages}
+          goToPrev={goToPrev}
+          goToNext={goToNext}
+        />
       </div>
 
       <div className="w-full rounded-lg">
         <h4 className="text-gray400 text-regular16 px-[28px]">이름</h4>
-        {members.map((member) => (
-          <div key={member.id} className="border-gray200 border-b">
-            <div className="flex justify-between px-[28px] py-[16px]">
-              <div className="flex items-center gap-[12px]">
-                <UserIcon width={38} height={38} />
-                <span>{member.nickname}</span>
-              </div>
-              <Button variant="ghost" size="delete" onClick={() => handleDeleteMember(member.id)}>
-                삭제
-              </Button>
-            </div>
-          </div>
-        ))}
+        <PaginationItems
+          data={members}
+          itemsPerPage={itemsPerPage}
+          currentPage={currentPage}
+          renderItems={(members) => (
+            <>
+              {members.map((member) => (
+                <>
+                  <div key={member.id} className="border-gray200 border-b">
+                    <div className="flex justify-between px-[28px] py-[16px]">
+                      <div className="flex items-center gap-[12px]">
+                        <UserIcon width={38} height={38} />
+                        <span>{member.nickname}</span>
+                      </div>
+                      <Button
+                        variant="ghost"
+                        size="delete"
+                        onClick={() => handleDeleteMember(member.id)}
+                      >
+                        삭제
+                      </Button>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </>
+          )}
+        />
       </div>
     </div>
   );
