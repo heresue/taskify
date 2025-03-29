8c1b51c8ad2d069a99469de5960c1dce9300abfd
heejin <heejin0857@naver.com>
2025-03-30 04:39:51 +0900

    Chore : typo

--- components/Dashboard/DashboardCard/DashboardCard.tsx
+++ components/Dashboard/DashboardCard/DashboardCard.tsx
@@ -17,12 +17,12 @@ interface CardProps {
 export default function Card({ imageUrl, title, tags, dueDate, profile }: CardProps) {
   const tag = separateTagColor(tags);
 
-  const defautImage = imageUrl === DEFAULT_CARD_IMAGE;
+  const defaultImage = imageUrl === DEFAULT_CARD_IMAGE;
 
   return (
     <div className="border-gray300 flex w-full cursor-pointer items-center justify-center rounded-md border border-solid bg-white p-3 sm:p-3 md:p-4 md:px-5 md:py-[18px]">
       <div className="flex w-full flex-col items-start justify-center gap-1 sm:flex-row sm:gap-5 lg:flex-col">
-        {!defautImage && (
+        {!defaultImage && (
           <div className="relative aspect-[260/152] h-auto w-full sm:h-[53px] sm:w-[90px] lg:h-40 lg:w-[274px]">
             <Image fill className="rounded-sm object-cover" src={imageUrl} alt="cardImage" />
           </div>
