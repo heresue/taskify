7946e285a3cf40aff856eb95bb1ca5c587481aaf
heresue <sue.synpark@gmail.com>
2025-03-29 16:18:15 +0900

    feat: CloseIcon 클릭 시 close 기능 적용

--- app/(dashboard)/dashboard/[dashboardId]/edit/InviteModal.tsx
+++ app/(dashboard)/dashboard/[dashboardId]/edit/InviteModal.tsx
@@ -20,7 +20,9 @@ export default function InviteModal({ isOpen, onClose }: Props) {
       <div className="w-full">
         <div className="flex items-center justify-between">
           <h3 className="text-bold24">초대하기</h3>
-          <CloseIcon width="36" height="36" />
+          <button onClick={onClose}>
+            <CloseIcon width="36" height="36" />
+          </button>
         </div>
         <label htmlFor="dashboardName" className="mt-6 mb-1 flex flex-col gap-2">
           <span className="text-medium18 text-black200 mb-2">이메일</span>
