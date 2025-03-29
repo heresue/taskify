829545f8f829393736e9b016470c65169725f845
Seok-jun.Kang <kangsj0327@gmail.com>
2025-03-29 17:40:00 +0900

    Fix : addBocIcon import 오류 해결

--- components/layout/Header/InvitationButton.tsx
+++ components/layout/Header/InvitationButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 import InviteModal from '@/app/(dashboard)/dashboard/[dashboardId]/edit/InviteModal';
-import AddBoxGrayIcon from '@/assets/icons/AddBoxGrayIcon';
+import AddBoxGrayIcon from '@/assets/icons/AddBoxIcon';
 import { useModal } from '@/hooks/useModal';
 
 export default function InvitationButton() {
@@ -12,7 +12,7 @@ export default function InvitationButton() {
         onClick={open}
         className="text-medium14 text-gray500 border-gray300 flex items-center gap-2 truncate rounded-md border-1 px-3 py-1.5 md:rounded-lg md:px-4 md:py-2"
       >
-        <AddBoxGrayIcon width={16} height={16} className="hidden md:block" />
+        <AddBoxGrayIcon width="16" height="16" className="hidden md:block" />
         초대하기
       </button>
       <InviteModal isOpen={isOpen} onClose={close} />
