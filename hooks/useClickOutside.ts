5e1df4383936ad82a68548dbf85cf9a2b509436a
Seok-jun.Kang <kangsj0327@gmail.com>
2025-03-29 17:52:46 +0900

    Feat : Add useClickOutside

---
+++ hooks/useClickOutside.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+export function useClickOutside<T extends HTMLElement>(
+  ref: React.RefObject<T>,
+  callback: () => void
+) {
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && event.target instanceof Node && !ref.current.contains(event.target)) {
+        callback();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref, callback]);
+}
