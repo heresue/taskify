ac32a858f9b180804e757b50c335fb52931d7324
heresue <sue.synpark@gmail.com>
2025-03-31 05:41:26 +0900

    refactor: CreateDashboardModal DashboardColorIcon을 ColorPalette 컴포넌트로 대체

--- app/(dashboard)/mydashboard/CreateDashboardModal.tsx
+++ app/(dashboard)/mydashboard/CreateDashboardModal.tsx
@@ -1,15 +1,22 @@
 import Input from '@/components/common/Input';
 import Modal from '@/components/common/Modal';
-import DashboardColorIcon, { COLOR_MAP } from '@/components/DashboardColorIcon/DashboardColorIcon';
+import ColorPalette from '@/components/Dashboard/ColorPalette';
+import { useState } from 'react';
 
 interface Props {
   isOpen: boolean;
   onClose: () => void;
 }
 
-const DASHBOARD_COLOR_KEYS = Object.keys(COLOR_MAP) as (keyof typeof COLOR_MAP)[];
+const DASHBOARD_COLORS = ['#ffa500', '#7ac555', '#76a5ea', '#e876ea', '#760dde'];
 
 export default function CreateDashboardModal({ isOpen, onClose }: Props) {
+  const [isSelected, setIsSelected] = useState(DASHBOARD_COLORS[0]);
+
+  const handleSelectColorClick = (color: string) => {
+    setIsSelected(color);
+  };
+
   return (
     <Modal
       isOpen={isOpen}
@@ -26,9 +33,11 @@ export default function CreateDashboardModal({ isOpen, onClose }: Props) {
           <Input id="dashboardName" name="dashboardName" placeholder="이름을 입력하세요" />
         </label>
         <div className="mb-6 flex gap-2">
-          {DASHBOARD_COLOR_KEYS.map((key) => (
-            <DashboardColorIcon key={key} size={30} colorKey={key} />
-          ))}
+          <ColorPalette
+            isSelected={isSelected}
+            colors={DASHBOARD_COLORS}
+            onColorSelect={handleSelectColorClick}
+          />
         </div>
       </div>
     </Modal>
