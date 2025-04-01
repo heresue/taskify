059a179f2bbd24b742df8e3b375c723dd6742003
Seok-jun.Kang <kangsj0327@gmail.com>
2025-04-01 16:50:51 +0900

    Style : fix mypage style

--- app/(dashboard)/mypage/page.tsx
+++ app/(dashboard)/mypage/page.tsx
@@ -6,11 +6,11 @@ import ArrowIcon from '@/assets/icons/ArrowIcon';
 export default function Page() {
   return (
     <div className="w-full max-w-[680px] px-3 py-4 md:p-4 lg:p-5">
-      <Link href="/mydashboard" className="flex w-fit items-center gap-2 md:hidden">
+      <Link href="/mydashboard" className="mb-5 flex w-fit items-center gap-2 md:hidden">
         <ArrowIcon width="16" height="16" />
         <span className="text-medium14 text-black200">돌아가기</span>
       </Link>
-      <Link href="/mydashboard" className="hidden w-fit items-center gap-2 md:flex">
+      <Link href="/mydashboard" className="mb-5 hidden w-fit items-center gap-2 md:flex">
         <ArrowIcon width="18" height="18" />
         <span className="text-medium16 text-black200">돌아가기</span>
       </Link>
