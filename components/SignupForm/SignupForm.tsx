b0342ede46e6fc9d488279e96a3af49cbc70e2a2
heejin <heejin0857@naver.com>
2025-04-02 01:48:51 +0900

    Refactor : add signup form rightIcon & cleanup code

--- components/SignupForm/SignupForm.tsx
+++ components/SignupForm/SignupForm.tsx
@@ -6,8 +6,30 @@ import FormField from '../compound/form/FormField';
 import Button from '../common/Button';
 import Modal from '../common/Modal';
 import useSignupForm from './useSignupForm';
-import Open from '@/public/icons/openEye.svg';
-import Close from '@/public/icons/closeEye.svg';
+import PasswordToggleBtn from './PasswordToggleBtn';
+
+const INPUT_FIELD_LIST = [
+  {
+    label: '이메일',
+    name: 'email',
+    placeholder: '이메일을 입력해 주세요',
+  },
+  {
+    label: '닉네임',
+    name: 'nickname',
+    placeholder: '닉네임을 입력해 주세요',
+  },
+  {
+    label: '비밀번호',
+    name: 'password',
+    placeholder: '8자 이상 입력해 주세요',
+  },
+  {
+    label: '비밀번호 확인',
+    name: 'checkPassword',
+    placeholder: '비밀번호를 한번 더 입력해 주세요',
+  },
+] as const;
 
 export default function SignupForm() {
   const {
@@ -23,39 +45,6 @@ export default function SignupForm() {
     isPending,
   } = useSignupForm();
 
-  const INPUT = [
-    {
-      label: '이메일',
-      name: 'email',
-      type: 'text',
-      placeholder: '이메일을 입력해 주세요',
-      value: formData.email,
-    },
-    {
-      label: '닉네임',
-      name: 'nickname',
-      type: 'text',
-      placeholder: '닉네임을 입력해 주세요',
-      value: formData.nickname,
-    },
-    {
-      label: '비밀번호',
-      name: 'password',
-      type: isPasswordVisible.password ? 'text' : 'password',
-      placeholder: '8자 이상 입력해 주세요',
-      value: formData.password,
-      isPassword: true,
-    },
-    {
-      label: '비밀번호 확인',
-      name: 'checkPassword',
-      type: isPasswordVisible.checkPassword ? 'text' : 'password',
-      placeholder: '비밀번호를 한번 더 입력해 주세요',
-      value: formData.checkPassword,
-      isPassword: true,
-    },
-  ];
-
   return (
     <form onSubmit={handleFormSubmit} className="flex w-full flex-col gap-6">
       <Modal
@@ -68,34 +57,37 @@ export default function SignupForm() {
         가입이 완료되었습니다!
       </Modal>
       <div className="flex flex-col gap-4">
-        {INPUT.map((input) => (
-          <div key={input.label} className="relative">
-            <FormField
-              name={input.name}
-              type={input.type}
-              value={input.value}
-              label={input.label}
-              placeholder={input.placeholder}
-              onChange={handleFormChange}
-              onKeyDown={handlePreventSpace}
-              isValid={state?.field !== input.name}
-              errorMessage={state?.field === input.name ? state.err : ''}
-              fieldType="input"
-            />
-            {input.isPassword &&
-              (isPasswordVisible[input.name] ? (
-                <Open
-                  onClick={() => toggleVisiblePassword(input.name)}
-                  className="absolute top-[70%] right-4 -translate-y-1/2 cursor-pointer"
-                />
-              ) : (
-                <Close
-                  onClick={() => toggleVisiblePassword(input.name)}
-                  className="absolute top-[70%] right-4 -translate-y-1/2 cursor-pointer"
-                />
-              ))}
-          </div>
-        ))}
+        {INPUT_FIELD_LIST.map((input) => {
+          const { label, name, placeholder } = input;
+          const isPasswordInput = ['password', 'checkPassword'].includes(name);
+          const isPasswordValueVisible = isPasswordVisible[name];
+          const isInputFieldValid = state?.field === name;
+
+          return (
+            <div key={label} className="relative">
+              <FormField
+                name={name}
+                type={isPasswordInput && isPasswordValueVisible ? 'text' : 'password'}
+                value={formData[name]}
+                label={label}
+                placeholder={placeholder}
+                onChange={handleFormChange}
+                onKeyDown={handlePreventSpace}
+                isValid={!isInputFieldValid}
+                errorMessage={isInputFieldValid ? state.err : ''}
+                fieldType="input"
+                rightIcon={
+                  isPasswordInput && (
+                    <PasswordToggleBtn
+                      isVisible={isPasswordValueVisible}
+                      onClick={() => toggleVisiblePassword(name)}
+                    />
+                  )
+                }
+              />
+            </div>
+          );
+        })}
         <CheckBox isChecked={formData.isChecked} handleIsChecked={handleIsChecked} />
       </div>
       <Button disabled={isPending || isFormIncomplete} type="submit" fullWidth size="auth">
